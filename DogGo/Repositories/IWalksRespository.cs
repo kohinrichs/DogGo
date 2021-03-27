110a63351259c9d1674a57185ac7eb1f086e2d9d
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-03-27 07:08:24 -0600

    Dog - View Model Components

---
+++ DogGo/Repositories/IWalksRespository.cs
@@ -0,0 +1,17 @@
+﻿using DogGo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public class IWalksRespository
+    {
+        List<Walk> GetAllWalks();
+        Walk GetWalksById(int id);
+        void AddWalks(Owner owner);
+        void UpdateOwner(Owner owner);
+        void DeleteOwner(int ownerId);
+    }
+}
